c41efc970fa2b5968c2d49fe0422af2c6ff6ebb4
Andrius <andriuss0316@gmail.com>
2021-07-08 16:54:01 +0300

    20210708

--- js/main.js
+++ js/main.js
@@ -2,7 +2,3 @@ const h1DOM = document.querySelector('h1');
 
 h1DOM.innerText = 'Labas vakaras!';
 
-//const a = "aaa"; 
-//const b = 'bbb';
-//const c = `ccc`;
-//kintamuju parasymo budai rasant su dvigubomis kabutemis, viengubomis, ir back tickais.
